e34677415c789ac80e2502c49d68efcaba79b51b
toqurJeon <wjswwkd@naver.com>
2026-03-24 23:23:47 +0900

    Update eks.tf
    alb feature updated

--- my-eks-project/eks.tf
+++ my-eks-project/eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "my-eks-cluster"
+  cluster_name    = "jeon-eks-cluster"
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
@@ -11,16 +11,28 @@ module "eks" {
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
+  enable_cluster_creator_admin_permissions = true
+  enable_irsa                              = true
+
   eks_managed_node_groups = {
-    general = {
+    for_pv = {
+      instance_types = ["t3.micro"]
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
+    }
+
+    app = {
       instance_types = ["t3.small"]
       min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      max_size       = 1
+      desired_size   = 1
+
+      labels = {
+        workload = "app"
+      }
     }
   }
-
-  enable_cluster_creator_admin_permissions = true
 
   cluster_security_group_additional_rules = {
     ingress_ec2_tcp_443 = {
