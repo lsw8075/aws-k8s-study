def2502b491c3a43f54e2a4609cce95a27ca4834
toqurJeon <wjswwkd@naver.com>
2026-03-31 21:44:03 +0900

    Update eks.tf

--- my-eks-project/eks.tf
+++ my-eks-project/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "jeon-eks-cluster"
-  cluster_version = "1.29"
+  cluster_version = "1.32"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
